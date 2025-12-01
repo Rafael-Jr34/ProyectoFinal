00d324ef70d3928928ae6a6e6c85eb19be0392a8
Rafael-Jr34 <rafaeljuniorsilfagomez@gmail.com>
2025-12-01 17:36:53 -0400

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,3 +1,13 @@
 "scripts": {
   "test": "echo \"Test OK\""
 }
+/*const request = require('supertest');
+const app = require('../src/index');
+
+describe('GET /', () => {
+  it('Debería responder Hola Mundo!', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('Hola Mundo!');
+  });
+});

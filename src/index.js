0fa97da4cdc32fdebdda295a3d47499c720078a9
Rafael-Jr34 <rafaeljuniorsilfagomez@gmail.com>
2025-12-01 17:03:40 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hola Mundo!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en puerto ${port}`);
+});
+
+module.exports = app;
---
+++ tests/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../src/index');
+
+describe('GET /', () => {
+  it('Debería responder Hola Mundo!', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('Hola Mundo!');
+  });
+});
